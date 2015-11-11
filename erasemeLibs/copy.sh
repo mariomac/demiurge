7070b66f7e48471ba5e0da4f942f49cc5731c82c
mariomac <mariomac@gmail.com>
2015-11-11 09:45:07 +0100

    Removed master

--- erasemeLibs/copy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cp ~/.m2/repository/eu/ascetic/utils/zabbix-api/0.1-SNAPSHOT/zabbix-api-0.1-SNAPSHOT.jar .
-cp ~/.m2/repository/eu/ascetic/iaas/energy-modeller/1.0-SNAPSHOT/energy-modeller-1.0-SNAPSHOT.jar .
-cp ~/.m2/repository/eu/ascetic/iaas/iaas-pricing-modeller/1.0-SNAPSHOT/iaas-pricing-modeller-1.0-SNAPSHOT.jar .
