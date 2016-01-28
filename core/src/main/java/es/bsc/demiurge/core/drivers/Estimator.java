2330103c60b734003e002e19b0eb959e4134bd95
mariomac <mariomac@gmail.com>
2016-01-28 17:05:04 +0100

    ASCETIC: Fixed issues with Energy modeler integration.

--- core/src/main/java/es/bsc/demiurge/core/drivers/Estimator.java
+++ core/src/main/java/es/bsc/demiurge/core/drivers/Estimator.java
@@ -2,6 +2,7 @@ package es.bsc.demiurge.core.drivers;
 
 import es.bsc.demiurge.core.clopla.domain.Host;
 import es.bsc.demiurge.core.clopla.domain.Vm;
+import es.bsc.demiurge.core.cloudmiddleware.CloudMiddlewareException;
 import es.bsc.demiurge.core.models.scheduling.VmAssignmentToHost;
 import es.bsc.demiurge.core.models.scheduling.DeploymentPlan;
 import es.bsc.demiurge.core.models.vms.VmDeployed;
@@ -30,7 +31,7 @@ public interface Estimator {
 	 * @param options
 	 * @return
 	 */
-	double getCurrentEstimation(String vmId, Map options); // for pricing modeler, options wil include the "false" value for undeployed
+	double getCurrentEstimation(String vmId, Map options) throws CloudMiddlewareException; // for pricing modeler, options wil include the "false" value for undeployed
 
 	/**
 	 * For clopla estimations. *CAUTION* the parameters are not VMM-specific host and vm classes but Clopla ones.
