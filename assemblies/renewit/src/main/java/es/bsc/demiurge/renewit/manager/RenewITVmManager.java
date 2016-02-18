ae90de1130b69071d9a2269c8178866ef1620628
Mauro Canuto <mauro.canuto88@gmail.com>
2016-02-18 14:34:41 +0100

    fixed

---
+++ assemblies/renewit/src/main/java/es/bsc/demiurge/renewit/manager/RenewITVmManager.java
@@ -0,0 +1,11 @@
+package es.bsc.demiurge.renewit.manager;
+
+import es.bsc.demiurge.core.manager.GenericVmManager;
+
+/**
+ * @author Mauro Canuto (mauro.canuto@bsc.es)
+ */
+public class RenewITVmManager extends GenericVmManager {
+
+    public class test{}
+}
