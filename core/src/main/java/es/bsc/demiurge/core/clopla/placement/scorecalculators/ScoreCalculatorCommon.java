4fa8460edfc439223132eff9e3b1c9a03a39c03d
raimonbosch <raimon.bosch@gmail.com>
2016-05-13 14:39:59 +0200

    Revert "fixing getClusterHardwareScore calculation to not compare with the list of hosts but the hosts assigned to each VM."
    This reverts commit 9c54151e5fe21f41ed702370994e69c74a1a3213.

--- core/src/main/java/es/bsc/demiurge/core/clopla/placement/scorecalculators/ScoreCalculatorCommon.java
+++ core/src/main/java/es/bsc/demiurge/core/clopla/placement/scorecalculators/ScoreCalculatorCommon.java
@@ -21,7 +21,6 @@ package es.bsc.demiurge.core.clopla.placement.scorecalculators;
 
 import es.bsc.demiurge.core.clopla.domain.ClusterState;
 import es.bsc.demiurge.core.clopla.domain.Host;
-import es.bsc.demiurge.core.clopla.domain.Vm;
 
 /**
  * This class includes score functions that are used in several score calculators.
@@ -43,8 +42,8 @@ public abstract class ScoreCalculatorCommon {
     
     public static double getClusterHardwareScore(ClusterState clusterState) {
         double result = 0;
-        for (Vm vm : clusterState.getVms()) {
-            if(vm.getHost() != null && !vm.getHost().matchesHardwareRequirements(vm)){
+        for (Host host: clusterState.getHosts()) {
+            if(!host.matchesHardwareRequirements(clusterState.getVms())){
                 result -= PENALTY_FOR_WRONG_HARDWARE;
             }
         }
